0f4c6f8cab638a8bb7738660be9f9fb2a7064e18
Tphegans <70431352+Tphegans@users.noreply.github.com>
2020-09-08 08:23:50 +0700

    2.2||operator aritmatika

---
+++ Sprint-1/topik-2/latihan-4.php
@@ -0,0 +1,21 @@
+<?php
+
+    $n = 5; 
+    $r = 2*$n+2;
+    echo "Nilai N=".$n." Nilai R=".$r;
+    $n=9;
+    $r = 2*$n+2;
+    echo "Nilai N=".$n." Nilai R=".$r;
+    $n = 19;
+    $r = 2*$n+2;
+    echo "Nilai N=".$n." Nilai R=".$r;
+
+    // for ($i=0; $i<=40; $i+=2) {
+    //     if ($i == 8) {
+    //         echo "Diketahui= <br>n=5, 9, 19<br> jika n5=nilai r adalah 8 <br>";
+    //     }if ($i==16) {
+    //         echo "Jika n9=nilai r adalah 16<br>";
+    //     }if ($i==36) {
+    //         echo "Jika n19=nilai r adalah 36";
+    //     }
+    // }
